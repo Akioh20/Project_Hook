b8ba9020ea5cb554f59b8942e21b54e8bd4808ca
Victor Romero <victor.romero.2@enti.cat>
2022-03-20 22:56:10 +0100

    Final commit to Levels, decorated and fixed some of them

---
+++ Assets/Project/Scripts/Volume.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+    public Slider slider;
+    public float sliderValue;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("audioVolume", 0.5f);
+        AudioListener.volume = slider.value;
+    }
+
+    public void ChangeSlider(float value)
+    {
+        sliderValue = value;
+        PlayerPrefs.SetFloat("audioVolume", sliderValue);
+        AudioListener.volume = slider.value;
+    }
+
+}
