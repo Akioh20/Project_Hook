31b6a83ba205ed2a4d084d8842efce9f950d795b
Victor Romero <victor.romero.2@enti.cat>
2023-02-08 02:00:49 +0100

    Added Sound To The Buttons

---
+++ Assets/Project/Scripts/PlayButtonSound.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButtonSound : MonoBehaviour
+{
+    public AudioSource clickSound;
+    public AudioSource hoverSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayClickSound()
+    {
+        clickSound.Play();
+    }
+    public void PlayHoverSound()
+    {
+        hoverSound.Play();
+    }
+}
